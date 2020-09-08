8199e6c5ca37a8807afe4fef469c7cac3cec2874
Zack Usso <qed@abbottanp.com>
2020-09-08 14:38:55 -0400

    Add files via upload

---
+++ 001_sloeber_wifi_vehicle/a038_jelly.h
@@ -0,0 +1,22 @@
+/*
+ * a038_jelly.h
+ *
+ *  Created on: Jun 1, 2020
+ *      Author: tom
+ */
+
+#ifndef A038_JELLY_H_
+#define A038_JELLY_H_
+
+
+//#include <Arduino.h>
+//#include <SoftwareSerial.h>
+#include "WiFly.h"
+
+
+
+#define _SSID "name_of_your_access_point"
+#define _PWD "password"
+
+
+#endif /* A038_JELLY_H_ */
