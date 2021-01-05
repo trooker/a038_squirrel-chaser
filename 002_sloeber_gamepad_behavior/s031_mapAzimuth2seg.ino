d4820b956ea3e18a4737f188ce4a03a0173645bf
Zack Usso <qed@abbottanp.com>
2021-01-04 20:19:22 -0500

    Revised downard the granularity of control
    Road test showed that the number of segments (24 for 360) was ideal but prohibitive in performance and caused excessive delay.  Instead a "sloppy" differential style steering supported the control of the vehicle within the expectations of a typical user who would be approaching a target of interest who might also be moving.  This sloppy steering resulted in the evolution of six "workable" segments.  See http://abbottanp.com/artifacts/squirrelChaser/page2.html#page2 Helpful Images showing Implemented as Shown Six Segment image and Mapped to Switch-Case image.

--- 002_sloeber_gamepad_behavior/s031_mapAzimuth2seg.ino
+++ 002_sloeber_gamepad_behavior/s031_mapAzimuth2seg.ino
@@ -4,7 +4,9 @@
  *
  * Reference arduino map()
  * https://www.arduino.cc/reference/en/language/functions/math/map/
- *
+ * 201213_tr Find a better segment split for controlling
+ *           Working for four segments with the 0 to 180/-170 to -1 line
+ *           splitting/centering the segments 1 and 3
  * 200929_tr Moved from spreadsheet analysis with 24 segments to assign
  *           turn control.
  *
@@ -12,24 +14,34 @@
 
 
 #include "Arduino.h"
-//The setup function is called once at startup of the sketch
-void setup()
-{
-	Serial.begin(9600);
 
-// Add your initialization code here
-}
 
-int tstAzimuth = -180;
-int seg;
+
+int tstAzimuth = -179;
+int seg = 3;
+int adj_seg;
 int const minDegree = -179;
 int const maxDegree =  180;
 int const minSeg    = 1;
-int const maxSeg    = 24;
+int const maxSeg    = 5;
+int const increment = 45;  // Degree increments for tstAzimuth
+int adj_val = maxSeg -1;
+
+
+void setup()
+{
+	Serial.begin(9600);
+	Serial.println(" ");
+	Serial.println("Mapping 360 Degrees to 4 segment of 90 degree each");
+	Serial.println("where 0 degrees mid-point for segment 1.");
+
+}
+
+
 bool doneflag = false;
 
 
-// The loop function is called in an endless loop
+
 void loop()
 {
 	if (!doneflag)
@@ -37,11 +49,39 @@ void loop()
 	int i = 0;
 	while (tstAzimuth < 181)
 	{
-		seg = map(tstAzimuth,minDegree, maxDegree, minSeg, maxSeg );
+        switch (tstAzimuth)
+        {
+        case   1 ... 45:
+        case -44 ... 0:
+		   Serial.println("-44 to 45");
+           seg = 1;
+           break;
+        case 46 ... 90:
+        case 91 ... 135:
+		   Serial.println("46 to 135");
+           seg = 2;
+           break;
+        case 136 ... 180:
+        case -135 ... -179:
+		   Serial.println("136 to -135");
+           seg = 3;
+           break;
+        case -134 ... -90:
+        case -89 ... -45:
+		   Serial.println("136 to -135");
+           seg = 4;
+           break;
+        }
+
+
+//		seg = map(tstAzimuth,minDegree, maxDegree, minSeg, maxSeg );
 		Serial.print(tstAzimuth);
-		Serial.print("  <== Azimuth       Segment ==> ");
+        Serial.print("  <== Azimuth       Segment ==> ");
 		Serial.println(seg);
-		tstAzimuth++;
+//		adj_seg = adj_val- seg;
+//		Serial.print("    Adjusted   Segment ==> ");
+//		Serial.println(adj_seg);
+		tstAzimuth+=increment;
 		doneflag = true;
 		i++;
 	}
